3c95aeb6d006e7eabd61716a731918e16d802c14
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-07-06 16:26:21 +0530

    Update voice_input.py

--- voice_input.py
+++ voice_input.py
@@ -1,1 +1,23 @@
 import speech_recognition as sr
+
+
+
+def listen_command():
+    recognizer =  sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        command = recognizer.recognize_google(audio)
+        print(f"You said: {command}")
+        return command.lower()
+    except sr.UnknownValueError:
+        print("Could not understand audio.")
+        return ""
+    except sr.UnknownValueError:
+        print("Could not understand audio.")
+    except sr.RequestError:
+        print("Speech service is unavailable.")
+        return ""
